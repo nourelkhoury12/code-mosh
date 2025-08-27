3140a21db07656af62195504e170b942f9329b05
nouremak <nour@emak.tech>
2025-08-27 18:10:18 +0300

    render compoment dynamically

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,15 @@
 function ListGroup(){
 
+    let items = ['New York', 'Paris', 'Tokyo', 'London'];
+
     return(
         <>
+        <h1>List Group</h1>
+        {items.length === 0 && <p>Items not founds</p>}
         <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
+            {items.map((item) => (
+                <li className="list-group-item" key={item}>{item}</li>
+            ))}
         </ul>
         </>
     );
