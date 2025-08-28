3d3be0cf9dfbe01d68c1950d604d54341f189154
nouremak <nour@emak.tech>
2025-08-28 15:29:30 +0300

    handle click event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,17 @@
+import { MouseEvent } from "react";
+
 function ListGroup(){
 
     let items = ['New York', 'Paris', 'Tokyo', 'London'];
 
+    const handleClikc = (event: MouseEvent) => console.log(event);
     return(
         <>
         <h1>List Group</h1>
         {items.length === 0 && <p>Items not founds</p>}
         <ul className="list-group">
-            {items.map((item) => (
-                <li className="list-group-item" key={item}>{item}</li>
+            {items.map((item,index) => (
+                <li className="list-group-item" onClick={handleClikc} key={item}>{item}</li>
             ))}
         </ul>
         </>
