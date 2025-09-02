ab669e9ab33250db771dc15cad4b40a7b1d8d5db
nouremak <nour@emak.tech>
2025-09-02 08:08:10 +0300

    14- showing and display and alert using state and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
   let items = ['New York', 'Paris', 'Tokyo', 'London'];
@@ -8,11 +9,14 @@ function App() {
   const handleSelectItem = (item: string) => {
     console.log(item)
   }
+
+  const [ alertVisible, setAlertVisible ] = useState(false);
+
   return (
     <div className="App">
-      <Alert>I need a Coffee <span>espresso</span></Alert>
       <ListGroup items={items} heading="Cities"  onSelectedItem={handleSelectItem}/>
-      <Button onClick={() => console.log("Clicked")}>Button</Button>
+      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>My alert</Alert>}
+      <Button onClick={() => setAlertVisible(true)}>Button</Button>
     </div>
   )
 }
